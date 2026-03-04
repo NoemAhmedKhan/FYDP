67e88f0edc6caf89c024711dfca9db21eba98286
NoemAhmedKhan <132838881+NoemAhmedKhan@users.noreply.github.com>
2026-03-05 04:29:26 +0500

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,4 @@
+// supabase.js — shared across all pages
+const SUPABASE_URL = 'https://ktzsshlllyjuzphprzso.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0enNzaGxsbHlqdXpwaHByenNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0MTg4ODksImV4cCI6MjA4Nzk5NDg4OX0.WMoLBWXf0kJ9ebPO6jkIpMY7sFvcL3DRR-KEpY769ic';
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
