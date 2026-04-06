882767993b5f98250ff846219629a3292649a921
NoemAhmedKhan <132838881+NoemAhmedKhan@users.noreply.github.com>
2026-04-06 14:02:40 +0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // supabase.js
 const { createClient } = window.supabase;
-const supabaseClient = createClient(
+const window.supabaseClient = createClient(
     'https://ktzsshlllyjuzphprzso.supabase.co',
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0enNzaGxsbHlqdXpwaHByenNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0MTg4ODksImV4cCI6MjA4Nzk5NDg4OX0.WMoLBWXf0kJ9ebPO6jkIpMY7sFvcL3DRR-KEpY769ic'
 );
