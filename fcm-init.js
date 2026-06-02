d955a26919295835a82809de6367b081da4ec801
Noem Ahmed Khan <132838881+NoemAhmedKhan@users.noreply.github.com>
2026-06-03 04:57:23 +0500

    Update fcm-init.js

--- fcm-init.js
+++ fcm-init.js
@@ -20,7 +20,7 @@
   const FCM_VAPID_KEY = "aPTek0pFhJ6EymLF0KeBv0iYq-7tY2Ogh2DJE1-Pi68";
 
   const SUPABASE_URL = 'https://ktzsshlllyjuzphprzso.supabase.co';
-  const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0enNzaGxsbHlqdXpwaHByenNvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MjQxODg4OSwiZXhwIjoyMDg3OTk0ODg5fQ.F8r1OZkP2fvwUS39H6LGt9JkZVQu4YHV50yusK5s1I4';
+  const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0enNzaGxsbHlqdXpwaHByenNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0MTg4ODksImV4cCI6MjA4Nzk5NDg4OX0.WMoLBWXf0kJ9ebPO6jkIpMY7sFvcL3DRR-KEpY769ic';
   const db = window.supabaseClient || window.supabase?.createClient(SUPABASE_URL, SUPABASE_KEY);
 
   // Only proceed if browser supports service workers
